95d360ed0b6977c01d0a6ea4927c20675b8a671e
MarkLux <marlx6590@163.com>
2017-07-17 11:19:27 +0800

    增加驼峰辅助函数

--- app/Common/Utils.php
+++ app/Common/Utils.php
@@ -44,4 +44,81 @@ class Utils
         $patternMobile = '/(13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7}/';
         return preg_match($patternMobile, $mobile) == 1;
     }
+
+    /**
+     * 判断数组是否是关联数组
+     * @param $array
+     * @return bool
+     */
+    public static function isAssoc($array)
+    {
+        if(is_array($array)) {
+            $keys = array_keys($array);
+            return $keys !== array_keys($keys);
+        }
+        return false;
+    }
+
+    /**
+     * 对一个变量进行下划线转驼峰化，可以是关联数组或者stdClass
+     * @param $var
+     * @param string $separator
+     * @return mixed
+     */
+    public static function camelize(&$var,$separator='_')
+    {
+        if ($var instanceof \stdClass) {
+            // 对对象的转换
+            return self::camelize(get_object_vars($var),$separator);
+        }
+
+        if (is_array($var)){
+            if (self::isAssoc($var)) {
+                $newKeys = [];
+                foreach ($var as $key => &$value){
+                    if (isset($newKeys[$key])) {
+                        continue;
+                    }
+                    $newKey = self::camelizeString($key,$separator);
+                    $newKeys[$newKey] = true;
+                    $var[$newKey] =  self::camelize($value,$separator);
+                    unset($var[$key]);
+                }
+            }
+        }
+        return $var;
+    }
+
+    public static function camelizeString($uncamelized_words,$separator='_')
+    {
+        $uncamelized_words = $separator. str_replace($separator, " ", strtolower($uncamelized_words));
+        return ltrim(str_replace(" ", "", ucwords($uncamelized_words)), $separator );
+    }
+
+    public static function unCamelize(&$var,$separator='_'){
+        if ($var instanceof \stdClass) {
+            // 对对象的转换
+            return self::unCamelize(get_object_vars($var),$separator);
+        }
+
+        if (is_array($var)){
+            if (self::isAssoc($var)) {
+                $newKeys = [];
+                foreach ($var as $key => &$value){
+                    if (isset($newKeys[$key])) {
+                        continue;
+                    }
+                    $newKey = self::unCamelizeString($key,$separator);
+                    $newKeys[$newKey] = true;
+                    $var[$newKey] =  self::unCamelize($value,$separator);
+                    unset($var[$key]);
+                }
+            }
+        }
+        return $var;
+    }
+
+    public static function unCamelizeString($camelCaps,$separator='_') {
+        return strtolower(preg_replace('/([a-z])([A-Z])/', "$1" . $separator . "$2", $camelCaps));
+    }
 }
