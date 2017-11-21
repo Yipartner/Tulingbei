50624203911896e32d00086873df90d659357ea1
hanxiao <1350690718@qq.com>
2017-11-21 17:58:42 +0800

    取消学校

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -51,9 +51,12 @@ class StudentController extends Controller
         ];
         if($request->school_type=='本校')
         {
+            return response()->json([
+                "code"=>"0",
+            ]);
             ValidationHelper::validateCheck($request->all(), $inrules);
             $studentInfo = ValidationHelper::getInputData($request, $inrules);
-//            $studentInfo['school']='东北大学秦皇岛分校';
+            $studentInfo['school']='东北大学秦皇岛分校';
         }
         else
         {
